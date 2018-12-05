a8213e1bec6142884bbd664eb9b6eda74d70d4e2
skywalkerwei <skywalkerwei@gmail.com>
2018-12-05 17:12:11 +0800

    增加docker 安装脚本   ---- for centos 7 +

--- docker_install.sh
+++ docker_install.sh
@@ -2,14 +2,49 @@
 # by skywalkerwei 2018.12.03
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
-LANG=en_US.UTF-
+LANG=en_US.UTF-8
+is64bit=`getconf LONG_BIT`
+#setup_path=/www
+
+if [ "$is64bit" != '64' ];then
+	echo "====================================="
+	echo "抱歉, 6.0不支持32位系统, 请使用64位系统!";
+	exit 0;
+fi
 
 command_exists() {
 	command -v "$@" > /dev/null 2>&1
 }
 
+
+Install_Check(){
+	while [ "$yes" != 'yes' ] && [ "$yes" != 'n' ]
+	do
+		echo -e "----------------------------------------------------"
+		echo -e "docker install tool"
+		echo -e "----------------------------------------------------"
+		read -p "输入yes强制安装/Enter yes to force installation (yes/n): " yes;
+	done 
+	if [ "$yes" == 'n' ];then
+		exit;
+	fi
+	if [ -z ${setup_path} ]; then
+    	setup_path=/www
+      	read -p "Default /www,Enter your choice : " setup_path
+      	if [ "${DISTRO}" = "Ubuntu" ]; then
+        	setup_path=/www
+    	fi
+    fi
+}
+
+
 do_install(){
+	Install_Check
 	yum -y update
+
+	yum install ntp -y
+	rm -rf /etc/localtime
+	ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 
 	if command_exists docker; then
 		echo "docker 已安装"
@@ -39,8 +74,8 @@ do_install(){
 		yum -y install git
 	fi
 	sudo gpasswd -a ${USER} docker
-	mkdir -p /www/
-	cd /www
+	mkdir -p $setup_path
+	cd $setup_path
 	git clone http://gl.ycpai.com/service/lnmp-docker.git
 	cd lnmp-docker
 	cp env.sample .env 
