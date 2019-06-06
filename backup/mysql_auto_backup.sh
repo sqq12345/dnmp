d84ac7b66abd673ea0fbb96c7d7cabc500373003
huangwei <skywalkerwei@gmail.com>
2019-06-06 17:57:05 +0800

    mysql 备份文件修改

--- backup/mysql_auto_backup.sh
+++ backup/mysql_auto_backup.sh
@@ -12,29 +12,27 @@
 # Shell Env
 SHELL_NAME="mysql_auto_backup.sh"
 
-BASE_PATH="/wwwroot/backup"
+BASE_PATH="DB"
 
 # get current time
 CURRENT_TIME=$(date '+%Y-%m-%d-%H:%M:%S')
 
 # get data eg：201911
-DIR_NAME=$(date -d yesterday +"%Y%m")
-
-# get days eg：03
-DAY=$(date -d yesterday +"%d")
-
+# DIR_NAME=$(date +%s)
+DIR_NAME=`date "+%Y-%m-%d"`  
 # db name
-DB_NAME="juhepay"
+DB_NAME="base"
 
 # backup path
-BACKUP_PATH=$BASE_PATH/$DB_NAME/$DIR_NAME
+BACKUP_PATH=$BASE_PATH/$DB_NAME/$DIR_NAME/
 
 # backup sql file name
-BACKUP_NAME=${DAY}".sql"
+BACKUP_NAME=${DB_NAME}".sql"
 
 # create log directory
 mkdir -p $BACKUP_PATH
 
 # run docker
-RES=$(docker exec dnmp-mysql mysqldump -uroot -p123456 $DB_NAME > $BACKUP_PATH/$BACKUP_NAME)
-echo $CURRENT_TIME-"备份结果："$? >> $BACKUP_PATH".log"
+RES=$(docker exec dnmp-mysql mysqldump  --defaults-extra-file=/etc/mysql/conf.d/mysql.cnf  $DB_NAME > $BACKUP_NAME)
+echo $CURRENT_TIME-"备份结果："$? 
+mv $BACKUP_NAME  $BACKUP_PATH
